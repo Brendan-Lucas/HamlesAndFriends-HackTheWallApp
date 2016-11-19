cf05b44eac3fd15608272d147835f9448b3fa7c1
JRichardsgit <jamesrichards@cmail.carleton.ca>
2016-11-19 15:25:02 -0500

    parser method

--- That's so Ravens/assets/parsing.py
+++ That's so Ravens/assets/parsing.py
@@ -7,8 +7,10 @@ def parsing(question):
     Answers_w = []
     for row in quiz:
         Questions.append(row[0])
+        Answers_c.append(row[1])
         Answers_w.append(row[2:5])
     f.close()
+    return Questions[question], Answers_c[question], Answers_w[question]
         
     
 
