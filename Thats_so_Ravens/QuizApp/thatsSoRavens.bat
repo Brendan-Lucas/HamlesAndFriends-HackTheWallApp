c527cc0653fa5e38ed335acf6f725393e4c1be94
Brendan Lucas <brendanwflucas@gmail.com>
2016-11-20 11:15:26 -0500

    Just Presented!

---
+++ Thats_so_Ravens/QuizApp/thatsSoRavens.bat
@@ -0,0 +1,2 @@
+start KinectV2MouseControl.exe
+start python C:\Users\BrendanLucas\Documents\GitHub\HamlesAndFriends-HackTheWallApp\Thats_so_Ravens\QuizApp\Run.py
--- Thats_so_Ravens/thatsSoRavens.bat
+++
@@ -1,2 +0,0 @@
-python C:\Users\BrendanLucas\Documents\GitHub\HamlesAndFriends-HackTheWallApp\Thats_so_Ravens\QuizApp\Run.py
-KinectV2MouseControl.exe
