86b5f693540179d10f067b637003bcced854abe6
JRichardsgit <jamesrichards@cmail.carleton.ca>
2016-11-20 06:53:46 -0500

    shit

--- That's so Ravens/maze.py
+++ That's so Ravens/maze.py
@@ -5,82 +5,139 @@ class Tile(pygame.sprite.Sprite):
                               pygame.sprite.Sprite.__init__(self)
                               self.image = pygame.Surface([width,height])
                               self.image.fill(color)
-                              self.reck = self.image.get_rect()
+                              self.rect = self.image.get_rect()
+                              
+                              
+class Rodney(pygame.sprite.Sprite):
+               def __init__(self):
+                              pygame.sprite.Sprite.__init__(self)
+                              self.image = pygame.image.load("assets/RodneySmall.jpg").convert()
+                              self.image.set_colorkey(white)
+                              self.rect = self.image.get_rect()
+                              
                               
 pygame.init()
 
 black=(0,0,0)
 white=(255,255,255)
-width = 200
-height = 200
-size = [700,700]
+width = 40
+height = 40
+size = [560,840] 
 screen = pygame.display.set_mode(size)
 
 tile_list = pygame.sprite.Group()
 
 #Tile 1
 
-tile1= Tile(white,width,height)
-pygame.draw.line(tile1.image,black,[0,0],[0,height],50)
-pygame.draw.line(tile1.image,black,[0,0],[width-1,0],50)
-tile_list.add(tile1)
+tileUp= Tile(white,width,height)
+pygame.draw.line(tileUp.image,black,[0,0],[0,height],10)
+pygame.draw.line(tileUp.image,black,[width-1,0],[width-1,height],10)
+tile_list.add(tileUp)
+
+
+#tile 2
+tileSide=Tile(white,width,height)
+pygame.draw.line(tileSide.image,black,[0,0],[width-1,0],10)
+pygame.draw.line(tileSide.image,black,[0,height],[width-1,height],10)
+tile_list.add(tileSide)
+
+
+#tile 3
+tileBotLeft= Tile(white,width,height)
+pygame.draw.line(tileBotLeft.image,black,[0,0],[0,height],10)
+pygame.draw.line(tileBotLeft.image,black,[0,height],[width-1,height],10)
+tile_list.add(tileBotLeft)
+
+
+#tile 4
+tileBotRight= Tile(white,width,height)
+pygame.draw.line(tileBotRight.image,black,[0,height],[width-1,height],10)
+pygame.draw.line(tileBotRight.image,black,[width-1,height],[width-1,0],10)
+tile_list.add(tileBotRight)
+
+
+#tile 5
+tileTopRight= Tile(white,width,height)
+pygame.draw.line(tileTopRight.image,black,[0,0],[width-1,0],10)
+pygame.draw.line(tileTopRight.image,black,[width-1,0],[width-1,height],10)
+tile_list.add(tileTopRight)
+
+
+#tile 6
+tileTopLeft= Tile(white,width,height)
+pygame.draw.line(tileTopLeft.image,black,[0,0],[width-1,0],10)
+pygame.draw.line(tileTopLeft.image,black,[0,0],[0,height],10)
+tile_list.add(tileTopLeft)
+
+
+
+#tile 7
+tileBlank = Tile(black,width,height)
+tile_list.add(tileBlank)
+
+# Rodney
+rodney = Rodney()
+
 
 
 going = True
 clock = pygame.time.Clock()
 
-while going:
-               clock.tick(10)
-               
-               for event in pygame.event.get():
-                              if event.type == pygame.QUIT:
-                                             going = False
-               screen.fill(white)
-               pygame.Surface.blit(tile1.image,[100,100])
-               
-               
-               ##maze tile 1 and 2
-               #image1 = pygame.Surface([width,height])
-               #image1.fill(white)
-               #pygame.draw.line(image1,black,[0,0],[0,height])
-               #pygame.draw.line(image1,black,[width,0],[width,height])
+lives = 3
 
-               pygame.display.flip()
+pygame.mouse.set_visible(False)
+while going:
+               pygame.mouse.set_pos(20,20)
+               alive = True
+               while alive:
+                              clock.tick(10)
+                              for event in pygame.event.get():
+                                             if event.type == pygame.QUIT:
+                                                            going = False
+                                                            
+                             
+                                             RodPos = pygame.mouse.get_pos()
+                                             x = RodPos[0]
+                                             y = RodPos[1]
+                                             screen.fill(white)
+                                             def rodneyhitsblack(x,y):
+                                                            if 1 < x < 546 and 1 < y < 819:
+                                                                           for i in (0,20):
+                                                                                          if screen.get_at((x-1, y+i))==black or screen.get_at((x+13, y+i))==black:
+                                                                                                         return True
+                                                                           for i in (0,13): 
+                                                                                          if screen.get_at((x+i, y-1))==black or screen.get_at((x+i, y+20))==black:
+                                                                                                         return True
+                                                            return False
+                                                            
+                                             
+                                             screen.blit(tileUp.image,[0,0])
+                                             
+                                             for i in array:
+                                                            for j in range(0,20):
+                                                                           if tunnels[i][j] == 0:
+                                                                                          screen.blit(tileBlank.image,[40*i,40*j])
+                                             
+                                             
+                                             
+                                             
+                                             
+                                             if rodneyhitsblack(x,y):
+                                                            alive = False
+                                                            tempx = x
+                                                            tempy = y
+                                                            RodPos = [tempx,tempy]
+                                                            lives = lives -1                                                           
+                                                            clk = False
+                                                            while clk == False:
+                                                                           for click in pygame.event.get():
+                                                                                          if click.type == pygame.MOUSEBUTTONDOWN:
+                                                                                                         clk = True
+                                                            if lives == 0:
+                                                                           going =False
+                                             
+                                             screen.blit(rodney.image,[x,y])
+                                             pygame.display.flip()
 pygame.quit()
                         
-##maze tile 3 and 4
-#image2 = pygame.Surface([width,height])
-#image1.fill(white)
-#pygame.draw.line(image2,color,[0,0],[width,0])
-#pygame.draw.line(image2,color,[0,height],[width,height])
-
-##maze tiel 5
-#image3 = pygame.Surface([width,height])
-#image1.fill(white)
-#pygame.draw.lines(image3,color,[[0,0],[0,height],[width,height]])
-
-##maze tile 6
-#image4 = pygame.Surface([width,height])
-#image1.fill(white)
-#pygame.draw.lines(image4,color,[[width,0],[witdh,height],[0,height]])
-
-##maze tile 7
-#image5 = pygame.Surface([width,height])
-#image1.fill(white)
-#pygame.draw.lines(image5,color,[[0,0],[width,0],[width,length]])
-
-##maze tile 8
-#image6 = pygame.Surface([width,height])
-#image1.fill(white)
-#pygame.draw.lines(image6,color,[[width,0],[0,0],[0,length]])
-
-
-
-
-#class Block(pygame.sprite.Sprite):
-               #def __init__(self, color, width, height):
-                       #super().__init__()
-                       
-                       #for event in pygame.event.get():
-                               #if event.type == pygame.QUIT:
-                                       #done == True
+#screen.blit(tileUp.image,[0,0])
--- That's so Ravens/rodney_cursor.py
+++ That's so Ravens/rodney_cursor.py
@@ -3,5 +3,5 @@ RodPos = pygame.mouse.get_pos()
 x = RodPos[0]
 y = RodPos[1]
 white = (255,255,255)
-screen.blit(Rodney[x,yy
+screen.blit(Rodney[x,y]
 Rodney.set_colorkey(white)
