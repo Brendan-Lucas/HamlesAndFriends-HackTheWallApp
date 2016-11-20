e22396090773b4a49b07ddbdb559f2892401e6e1
Brendan Lucas <brendanwflucas@gmail.com>
2016-11-20 01:05:59 -0500

    SemiFunctioningGUI

--- That's so Ravens/QuizApp/gui.py
+++ That's so Ravens/QuizApp/gui.py
@@ -16,21 +16,22 @@ done = False
 
 clock = pygame.time.Clock()
 
-def makebutton(displayFrame,colour, left,top,width,height, Answer):
+
+def makebutton(displayFrame, colour, left, top, width, height, Answer):
 
     mouse = pygame.mouse.get_pos()
-    #whiteButton = pygame.image.load("quizAssets/ImagesForQuizApp/Button_silver.jpg")
     
-    #blueButton = pygame.image.load("Button_purple.png").convert()
+    #blueButton = pygame.image.load("Button_purple.jpg").convert()
     #greenButton = pygame.image.load("ImagesForQuizApp/Button_green.png").convert()
     #redButton = pygame.image.load("ImagesForQuizApp/Button_red.png").convert()    
-
+    whiteButton = pygame.image.load("quizAssets/ImagesForQuizApp/Button_silver.jpg")
     if left+width > mouse[0] > left and top < mouse[1] < top+height:
-        print 'mouse'
+
+                
         if event.type == pygame.MOUSEBUTTONDOWN:
-            print 'button'
+            print 'button' + str(mouse)
     else:
-        rect = pygame.draw.rect(displayFrame,colour, (left,top,width,height))  
+        rect = pygame.draw.rect(displayFrame, colour, (left, top, width, height))  
 
     X = left+(width/2)
     Y = top+(height/2)      
@@ -42,6 +43,8 @@ def makebutton(displayFrame,colour, left,top,width,height, Answer):
     #gameDisplay.blit(TextSurf,TextRect)
 
 
+zFrame_background = pygame.image.load("quizAssets\ImagesForQuizApp\QuizBackgroundAdjust.jpg")
+backgroundRect=zFrame_background.get_rect()  
 
 while not done:
     for event in pygame.event.get():
@@ -53,11 +56,12 @@ while not done:
             done = True
         #elif event.type == p
         
-        screen.fill(WHITE)
-        makebutton(screen, GREEN, 100, 400, 300, 200, 'plop')
+        
+        screen.blit(zFrame_background, backgroundRect)
+        makebutton(screen, WHITE, 100, 400, 300, 200, 'plop')
         
         pygame.display.flip()
         
         clock.tick(60)
-        
-    
+
+pygame.quit()
