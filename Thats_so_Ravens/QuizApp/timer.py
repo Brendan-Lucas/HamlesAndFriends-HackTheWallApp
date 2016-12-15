7e5b386b50e4cf616d3aa90932815a5c0b01f984
Brendan Lucas <brendanwflucas@gmail.com>
2016-12-14 21:54:08 -0800

    Fixed Timer, with multithreading and object locking
    -synchronized print method to reduce call frequency,
    -multithreaded print function and timer function.
    -Timer fully functional

--- Thats_so_Ravens/QuizApp/timer.py
+++ Thats_so_Ravens/QuizApp/timer.py
@@ -1,8 +1,12 @@
 import pygame
+#!/usr/bin/python
 import time
+import thread
 pygame.init()
 
 class Timer:
+    black=(0,0,0)
+    white=(255,255,255)    
     def __init__(self,clockFace,display,centerPosX,centerPosY):
         #an "image" directory should be passed for clockFace
         self.font = pygame.font.SysFont('Calibri', 35, True, False)
@@ -12,50 +16,62 @@ class Timer:
         self.currentTime = ''
         self.display = display
         self.centerPos = (centerPosX,centerPosY)
+        self.lock = thread.allocate_lock()
+        self.running = False
         
         
     def runTimer(self, startTime, endTime):
         self.startTime = startTime   
         self.currentTime = self.startTime
-        while self.currentTime != self.endTime:
-            time.sleep(1)      
+        self.running = True
+        while self.currentTime != self.endTime and self.running:
+            self.lock.acquire()
+            time.sleep(1)
             if self.startTime < self.endTime:
                 self.currentTime += 1
+                print(self.currentTime)
             else:
                 self.currentTime -=1
+            self.lock.release()
+        self.running = False
             
     def printTime(self):
         fontValue = ''
         if self.currentTime<60: fontValue += '0:'
         if self.currentTime<10: fontValue += '0'
         fontValue += str(self.currentTime)
-        timeValue = self.font.render(fontValue,True,black)  
+        timeValue = self.font.render(fontValue, True, black) 
+        self.display.fill(white)
         self.display.blit(timeValue,self.centerPos)
         # self.display.blit(self.clockFace,self.centerPos)
     
-    
+    def synchronizedPrintTime(self):
+        while (self.running):
+            self.lock.acquire() #Ask me to explain locking and Synchronizing if you need to,
+            self.printTime()    #the internet often overcomplicates the ideas. 
+            self.lock.release()
     
 black=(0,0,0)
 white=(255,255,255)
 
 size = [560,840] 
 screen = pygame.display.set_mode(size)
-time = Timer('quizAssets/ImagesForQuizApp/Button_purple.png', screen, 100, 100)
-time.runTimer(20,0)
+screen.fill(white)
+pygame.display.flip()
+timer = Timer('quizAssets/ImagesForQuizApp/Button_purple.png', screen, 100, 100)
 going = True
+thread.start_new_thread(timer.runTimer, (20, 0))
+thread.start_new_thread(timer.synchronizedPrintTime, ())
 while going:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             going = False
-        screen.fill(white)
-        print("i get here")
-        time.printTime()
-        print("fuck you")
+            timer.running = False
+    pygame.display.flip()
 
-        pygame.display.flip()
-
+timer.lock.acquire()
 pygame.quit()
     
-
+###PLS note ask me to explain locking if you need to, The internet overcomplicates the the idea. 
     
     
