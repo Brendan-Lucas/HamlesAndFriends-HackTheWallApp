9b24a65203923467af4776d0b55648b61afe3955
Brendan_Lucas <brendanwflucas@gmail.com>
2017-01-05 11:24:55 -0800

    refactored quiz run code for scaling and editing purposses

--- Thats_so_Ravens/QuizApp/quiz.py
+++ Thats_so_Ravens/QuizApp/quiz.py
@@ -1,6 +1,6 @@
-import collections
 import pygame
 import Thats_so_Ravens.Helpers as helpers
+import Thats_so_Ravens.info
 from timer import Timer
 import parsing as parse
 from pygame import *
@@ -21,7 +21,10 @@ class Quiz:
         self.screen = pygame.display.set_mode(size)
         self.score = 0
         self.q_count = 0
+        self.clock = pygame.time.Clock()
         self.b_press = False
+        self.back = False
+        self.done = False
         self.switch_q = False
         self.questions = []
         self.file=" hamlesFile "
@@ -33,7 +36,6 @@ class Quiz:
         self.timeoutTimer = Timer()
         self.calibri_35 = pygame.font.SysFont('Calibri', 35,True,False)
         self.calibri_18 = pygame.font.SysFont('Calibri', 18,True,False)
-        self.clock = pygame.time.Clock()
         self.background = self.init_and_resize_image('interim-background.png', self.screen.get_size())
         self.greenButton = self.init_and_resize_image('green_button_free.png', (helpers.normalize(self.size, helpers.normalize(self.size, 220, 'x'), 'x'),helpers.normalize(self.size, 132, 'y')))
         self.redButton = self.init_and_resize_image('red_button_free.png', (helpers.normalize(self.size, 220, 'x'),helpers.normalize(self.size, 132, 'y')))
@@ -62,37 +64,33 @@ class Quiz:
         for i in range(0,5):
             self.questions.append(Question().init_question(Qarr[i], Aarr[i]))
 ############## End of INIT HELPERS
-
-    def make_question(self):
-        def split_question_print_text(text, arr):
-            if(len(text)>25 and text.find(' ', 21)!=-1):
-                num = text.find(' ', 25) if 0<text.find(' ', 25)<=26 else text.find(' ', 21)
-                arr.append(self.calibri_35.render(text[:num],True,BLACK))
-                return split_question_print_text(text[num:], arr)
-            else:
-                arr.append(self.calibri_35.render(text,True,BLACK))
-                return arr
-
+    #Make questions fit within size provided and blit question lines to screen font 35
+    # --requires background blit before
+    def make_question(self, font=None):
+        if font is None: font = self.calibri_35
         base_q_coordinates = [X, Y] = [60, 290]
         augment_coordinates = [Xa, Ya] = [0, 32]
         text = self.questions[self.q_count].get_question() if self.q_count < len(self.questions) else  "Finished, Hope you passed."
-        print_statement = split_question_print_text(text, [])
+        print_statement = helpers.split_question_print_text(text, [], font)
         for i in range(0,len(print_statement)):
-            self.screen.blit(print_statement[i], [X, Y + Ya*i])
-
+            self.screen.blit(print_statement[i], [X + Xa*i, Y + Ya*i])
+    #Blit Score: self.score to screen in top right corner font 18
+    # --requires blit background first
     def make_score(self):
         score_coordinates = [helpers.normalize(self.size, 475, 'x'), helpers.normalize(self.size, 30, 'y')]
         temp_text="Score: "+str(self.score)
         score_text = self.calibri_18.render(temp_text, True, BLACK)
         self.screen.blit(score_text, score_coordinates)
 
-    def make_back(self, event):
+    #Blits back button to screen and listens for action,
+    # -- hopefully replaced by action listener.
+    def make_back(self, event=None):
         back_pressed=True
-        mouse = pygame.mouse.get_pos()
         x_center = helpers.normalize(self.size, 30, 'x')
         y_center = helpers.normalize(self.size, 30, 'y')
         side = 40
         coordinates_button=(x_center-(side/2), y_center-(side/2))
+        mouse = pygame.mouse.get_pos()
         if (x_center-(side/2) < mouse[0] < x_center+(side/2)) and (y_center-(side/2) < mouse[1] < y_center+(side/2)):
             self.screen.blit(self.backButtonBlue, coordinates_button)
             if event:
@@ -138,75 +136,84 @@ class Quiz:
                 rect = self.screen.blit(self.redButton, [left, top])
                 self.screen.blit(text, coordinates_text)
 
-
-    def fresh_screen(self, event=False):
-        backgroundRect=self.background.get_rect()
-        self.screen.blit(self.background, self.background.get_rect())
-        self.make_score()
-        self.make_question()
-        if event:
-            self.make_answers(event)
-
-    def run_if(self, event=None):
-        back = self.make_back(event)
-        if self.q_count < len(self.questions) and not self.timer2.running:
-            self.make_answers(event)
-        if not self.b_press and not self.timer.running:
-            self.cor_img = self.init_and_resize_image('wrong.png', (helpers.normalize(self.size, 300, 'x'), helpers.normalize(self.size, 100, 'y')))
-            if self.q_count<len(self.questions): self.b_press = True
-        if self.b_press and not self.switch_q:
-            self.timer.stop()
-            self.fresh_screen(event)  # setsScoreToNewValue
-            self.make_answers()
-            self.screen.blit(self.cor_img, [helpers.normalize(self.size, 130, 'x'), helpers.normalize(self.size, 390, 'y')])
-            pygame.display.flip()
-            self.timer2.runTimer(3, 0)
-            self.switch_q = True
-        if self.switch_q and not self.timer2.running:
-            self.q_count += 1
-            self.b_press = False
-            self.switch_q = False
-            self.fresh_screen()
-            if self.q_count < len(self.questions): self.timer.runAndPrintTimer(30, 0)
-
-    def run_screen(self):
-        pygame.display.set_caption("Try and pass ECOR 1010, In Mcrae We Trust")
-        self.switch_q = False
-        this_font = pygame.font.SysFont('Calibri', 25, True, False)
-        self.timeout = False
-        back = False
-        done = False
-        self.fresh_screen()
-        if self.q_count<len(self.questions): self.timer.runAndPrintTimer(30, 0)
-        self.timeoutTimer.runTimer(90, 0)
-        while not (back or done or self.timeout):
+    def answer_stage(self, image):
+        self.timer.stop()
+        self.timer2.runTimer(2, 0)
+        self.init_screen()
+        self.screen.blit(self.cor_img, [helpers.normalize(self.size, 130, 'x'), helpers.normalize(self.size, 390, 'y')])
+        self.make_answers()
+        pygame.display.flip()
+        while self.timer2.running:
             for event in pygame.event.get():
                 self.timeoutTimer.currentTime = 90
                 if event.type == pygame.QUIT:
-                    done=True #true or false value
-                    break
+                    self.done = True
+                    return self.done
                 #elif event.type == pygame.MOUSEBUTTONDOWN:
                     #click_sound.play()
-                back = self.make_back(event)
-                if self.q_count<len(self.questions) and not self.timer2.running:
-                    self.make_answers(event)
-                if not self.b_press and not self.timer.running:
-                    self.cor_img = self.init_and_resize_image('wrong.png', (helpers.normalize(self.size, 300, 'x'), helpers.normalize(self.size, 100, 'y')))
-                    if self.q_count<len(self.questions): self.b_press = True
-                if self.b_press and not self.switch_q:
-                    self.timer.stop()
-                    self.fresh_screen(event)  # setsScoreToNewValue
-                    self.screen.blit(self.cor_img, [helpers.normalize(self.size, 130, 'x'), helpers.normalize(self.size, 390, 'y')])
-                    pygame.display.flip()
-                    self.timer2.runTimer(3, 0)
-                    self.switch_q = True
-                #after time passed, want to go to bulrb screen, contians image top left and info below
+                self.back = self.make_back(event)
+                if self.back:
+                    return self.done
                 pygame.display.flip()
-            #outside for
-            self.run_if()
-            pygame.display.flip() #possibly not needed
-            if not self.timeoutTimer.running:
-                self.timeout = True
-        return done
+        return False
+
+
+    def init_screen(self):
+        self.screen.blit(self.background, self.background.get_rect())
+        self.make_score()
+        self.make_back()
+
+    #blit background, score, question, make answer buttons and start timer at 20
+    def fresh_screen(self):
+        self.screen.blit(self.background, self.background.get_rect())
+        self.make_back()
+        self.make_score()
+        self.make_question()
+        self.make_answers()
+        self.timer.runAndPrintTimer(20, 0)
+
+    def run_screen(self):
+        pygame.display.set_caption("Try and pass ECOR 1010, In Mcrae We Trust")
+        self.timeout = False
+        self.back = False
+        self.done = False
+        self.timeoutTimer.runTimer(90, 0)
+        #first Info Box explaining quiz runInfo(time)
+        while self.q_count < len(self.questions):
+            self.switch_q=False #TODO: look at changing to local instead of instance
+            self.fresh_screen()
+            temp = self.q_count
+            while not self.switch_q and self.timeoutTimer.running:
+                self.clock.tick(60)
+                for event in pygame.event.get():
+                    self.timeoutTimer.currentTime = 90
+                    if event.type == pygame.QUIT:
+                        self.done = True
+                        return self.done
+                    #elif event.type == pygame.MOUSEBUTTONDOWN:
+                        #click_sound.play()
+                    self.back = self.make_back(event)
+                    if self.back:
+                        return self.done
+                    if not self.timer2.running: self.make_answers(event) #TODO may not need self.timer2.running in there
+                    if not self.b_press and not self.timer.running:
+                        self.b_press=True
+                        self.cor_img = self.init_and_resize_image('wrong.png', (helpers.normalize(self.size, 300, 'x'), helpers.normalize(self.size, 100, 'y')))
+                    if self.b_press: break
+                    pygame.display.flip()
+                #outside For
+                if not self.b_press and not self.timer.running:
+                    self.b_press=True
+                    self.cor_img = self.init_and_resize_image('wrong.png', (helpers.normalize(self.size, 300, 'x'), helpers.normalize(self.size, 100, 'y')))
+                if self.b_press:
+                    if self.answer_stage(self.cor_img): return self.done
+                    #run info blitting the info to the surface that we already have so that we can continue to update back button and pygame quit
+                    #self.run_info(self.screen)
+                    self.b_press = False
+                    self.switch_q = True
+                    self.q_count+=1
+            if not self.timeoutTimer.running: break
+        return self.done
+
 #click_sound = pygame.mixer.Sound("laser5.ogg")
 #click_sound.play
