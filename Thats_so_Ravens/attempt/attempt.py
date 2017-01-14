938a6b50f5682362bcc48eb26c7f2cd042f14a89
Brendan_Lucas <brendanwflucas@gmail.com>
2017-01-13 17:15:25 -0800

    Attempt succesfful in loading textures and models

--- Thats_so_Ravens/attempt/attempt.py
+++ Thats_so_Ravens/attempt/attempt.py
@@ -9,13 +9,27 @@ class AttemptLoad(ShowBase):
 
     def __init__(self):
         ShowBase.__init__(self)
+
+        #add Ambient Light
         ambientLight = AmbientLight("AmbLight")
         ambientLight.setColor(Vec4(1, 1, 1, 1))
         ambientLightNodePath = self.render.attachNewNode(ambientLight)
         self.render.setLight(ambientLightNodePath)
-        self.setBackgroundColor(0, 0, 0)
+        #set Background colour.
+        #self.setBackgroundColor(0, 0, 0)
+        #load Ring Model
         self.ring = self.loader.loadModel('models/ring')
         self.ring.reparentTo(self.render)
+        self.ring.setPos(0,0,0)
+
+        self.cam.setPos(0, -32, 10)
+
+
+        self.arod = self.loader.loadModel('models/A_Rod_Mod')
+        self.arod.setScale(0.001, 0.001, 0.001)
+        self.arod.reparentTo(self.ring)
+        self.arod.setPos(0, 0, 4.5)
+
 
 attempt = AttemptLoad()
 attempt.run()
