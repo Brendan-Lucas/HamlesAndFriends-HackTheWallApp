165c8b99f91d11bc0e765b13ddd85891ca745e4d
Brendan_Lucas <brendanwflucas@gmail.com>
2017-01-15 22:44:20 -0800

    added bat file

---
+++ runRavensRun.bat
@@ -0,0 +1,2 @@
+START "" "C:\Users\brendanlucas\Documents\GitHub\HamlesAndFriends-HackTheWallApp\kinectInterfacingBen\ConsoleApplication3\obj\Debug\ConsoleApplication3.exe"
+CALL python Thats_so_Ravens/Run.py
