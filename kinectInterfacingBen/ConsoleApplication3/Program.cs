da0fe081dc993cb36cfa3c7a838fb963ff1eaef0
Benjamin-Bichel <bichel10@hotmail.com>
2017-01-15 23:29:09 -0500

    Hey its Ben I did a thing

---
+++ kinectInterfacingBen/ConsoleApplication3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        public static bool end = false;
+        static void Main(string[] args)
+        {
+            KinectControl a = new KinectControl();
+            bool done = false;
+            while (done == false)
+            {
+               
+                done = end;
+            }
+                        
+        }
+
+
+
+
+        private void FormMain_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                MessageBox.Show("e");
+            }
+        }
+    }
+}
