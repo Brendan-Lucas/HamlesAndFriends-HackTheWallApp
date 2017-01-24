a45c4bdc1408bc967cf3a5b948f293c80b376171
Benjamin-Bichel <bichel10@hotmail.com>
2017-01-24 00:21:17 -0500

    Make the batch file work

--- runRavensRun.bat
+++ runRavensRun.bat
@@ -1,2 +1,2 @@
-START "" "C:\Users\brendanlucas\Documents\GitHub\HamlesAndFriends-HackTheWallApp\kinectInterfacingBen\ConsoleApplication3\obj\Debug\ConsoleApplication3.exe"
+START "" "kinectInterfacingBen\ConsoleApplication3\obj\Debug\ConsoleApplication3.exe"
 CALL python Thats_so_Ravens/Run.py
